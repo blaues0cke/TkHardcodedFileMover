a357c1d14ba40b6fb2a5a307c52ee665e52b23e1
blaues0cke <thomas@thomaskekeisen.de>
2013-08-04 13:47:06 +0200

    Initial commit.

---
+++ TkHardcodedFileMover/TkHardcodedFileMover/TkHardcodedFileMover/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TkHardcodedFileMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Panel3 = new System.Windows.Forms.Panel();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Panel2.SuspendLayout();
+            this.Panel1.SuspendLayout();
+            this.Panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBox1.Location = new System.Drawing.Point(10, 10);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(893, 497);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // Panel2
+            // 
+            this.Panel2.Controls.Add(this.TextBox1);
+            this.Panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Panel2.Location = new System.Drawing.Point(0, 0);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.Panel2.Size = new System.Drawing.Size(913, 517);
+            this.Panel2.TabIndex = 3;
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.ProgressBar1);
+            this.Panel1.Controls.Add(this.Panel3);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panel1.Location = new System.Drawing.Point(0, 517);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.Panel1.Size = new System.Drawing.Size(913, 44);
+            this.Panel1.TabIndex = 2;
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProgressBar1.Location = new System.Drawing.Point(10, 10);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(811, 24);
+            this.ProgressBar1.TabIndex = 1;
+            // 
+            // Panel3
+            // 
+            this.Panel3.Controls.Add(this.Button1);
+            this.Panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Panel3.Location = new System.Drawing.Point(821, 10);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Size = new System.Drawing.Size(82, 24);
+            this.Panel3.TabIndex = 0;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(6, 1);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Start";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 561);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Panel2.ResumeLayout(false);
+            this.Panel2.PerformLayout();
+            this.Panel1.ResumeLayout(false);
+            this.Panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Panel Panel2;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+        internal System.Windows.Forms.Panel Panel3;
+        internal System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ TkHardcodedFileMover/TkHardcodedFileMover/TkHardcodedFileMover/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TkHardcodedFileMover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            moveToRaumfeld(@"C:\Dropbox\@home\Raumfeld");
+            moveToNAS(@"C:\Dropbox\@home\Raumfeld");
+            moveToNAS(@"C:\Dropbox\@home\NAS");
+
+
+
+        }
+
+
+        private void moveTo(String sDir, String tDir, bool deleteAfterCopy)
+        {
+            ProgressBar1.Maximum = 0;
+            FileInfo theFileInfo = new FileInfo(sDir);
+            string[] dirs = Directory.GetDirectories(sDir);
+            ProgressBar1.Maximum += dirs.Length;
+            addLog("Processing folder " + ProgressBar1.Value.ToString() + " of " + ProgressBar1.Maximum.ToString());
+
+            foreach (string dirName in dirs)
+            {
+                string shortDirName = new DirectoryInfo(dirName).Name;
+
+                ++ProgressBar1.Value;
+                addLog("Processing folder " + ProgressBar1.Value.ToString() + " of " + ProgressBar1.Maximum.ToString() + ": " + dirName + " (" + shortDirName + ")");
+
+                string dst = tDir + shortDirName;
+
+                addLog("Copying " + dirName + " to " + dst);
+                Application.DoEvents();
+                copyDirectory(dirName, dst);
+
+                Application.DoEvents();
+                addLog("Done!");
+
+
+                if (deleteAfterCopy)
+                {
+                    delteDir(dirName);
+                }
+
+            }
+
+            
+        }
+
+        private void delteDir(String dir)
+        {
+            addLog("Deleting " + dir);
+    
+           
+            try
+            {
+                Directory.Delete(dir, true);
+            }
+            catch (IOException)
+            {
+                Thread.Sleep(10);
+                Directory.Delete(dir, true);
+            }
+        }
+
+        private void moveToRaumfeld(String sDir)
+        {
+            moveTo(sDir, @"Z:\", false);
+        }
+
+        private void moveToNAS(String sDir)
+        {
+            moveTo(sDir, @"M:\", true);
+        }
+
+        private void addLog(String log)
+        {
+            TextBox1.Text = log + "\r\n" + TextBox1.Text;
+        }
+
+        private void copyDirectory(String from, String to)
+        {
+            
+//Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(from, "*",
+    SearchOption.AllDirectories))
+            {
+                addLog("Copying directory " + from + " to " + to);
+                Application.DoEvents();
+                Directory.CreateDirectory(dirPath.Replace(from, to));
+            }
+
+//Copy all the files
+            foreach (string newPath in Directory.GetFiles(from, "*.*",
+    SearchOption.AllDirectories))
+            {
+
+
+                addLog("Copying " + newPath + " to " + newPath.Replace(from, to));
+                Application.DoEvents();
+
+                File.Copy(newPath, newPath.Replace(from, to), true);
+            }
+        }
+
+        // 
+        //
+    }
+}
